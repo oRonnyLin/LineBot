abd79f93b80b822c7cb28a75cfe018c21395d8a4
Ronny Lin <ronnylin10@gmail.com>
2020-04-21 22:54:21 -0700

    data aggregation

--- test.js
+++ test.js
@@ -65,58 +65,78 @@ async function writeFileProvincial () {
   console.log('wrote file')
 }
 
+function calculateAverage (arr, field) {
+  let count = 0
+  const reducerFuncBase = (acu, cur) => {
+    if (!isFinite(cur) || cur === undefined) {
+      return acu
+    } else {
+      count++
+      return acu + cur
+    }
+  }
+  const result = arr.reduce((acu, cur) => { return reducerFuncBase(acu, cur[field]) }, 0) / count
+  return result
+}
+
 function readCSVFile () {
   return new Promise((resolve, reject) => {
-    const result = []
+    let prevDataBC = null
+    let prevDataCA = null
+    const resultBC = []
+    const resultCA = []
     fs.createReadStream('test.csv')
       .pipe(csv())
       .on('data', (data) => {
-        const { pruid, date, prname, numconf, numtested, numrecover, percentrecover, numtoday, percentoday } = data
+        const { pruid, date, prname, numconf, numprob, numtested, numrecover, percentrecover, numtoday, percentoday } = data
         if (pruid === '59' || pruid === '1') { // BC & Canada
-          console.log(data)
-          result.push({
+          const curData = {
             date: date,
             name: prname,
-            numconf: parseInt(numconf),
+            numconf: parseInt(numconf) + parseInt(numprob),
             numtested: parseInt(numtested),
             numrecover: parseInt(numrecover),
             percentrecover: parseFloat(percentrecover),
             numtoday: parseInt(numtoday),
             percentoday: parseInt(percentoday)
-          })
+          }
+          if (pruid === '59') {
+            if (prevDataBC) {
+              curData.newCasesToday = curData.numconf - prevDataBC.numconf
+              curData.newRecover = curData.numrecover - prevDataBC.numrecover
+              curData.newTested = curData.numtested - prevDataBC.numtested
+              curData.percentCase = ((curData.newCasesToday / prevDataBC.numconf) * 100)
+              curData.percentRec = ((curData.newRecover / prevDataBC.numrecover) * 100)
+              curData.positiveRate = ((curData.newCasesToday / curData.newTested) * 100)
+            }
+            prevDataBC = curData
+            resultBC.push(curData)
+          } else if (pruid === '1') {
+            if (prevDataCA) {
+              curData.newCasesToday = curData.numconf - prevDataCA.numconf
+              curData.newRecover = curData.numrecover - prevDataCA.numrecover
+              curData.newTested = curData.numtested - prevDataCA.numtested
+              curData.percentCase = ((curData.newCasesToday / prevDataCA.numconf) * 100)
+              curData.percentRec = ((curData.newRecover / prevDataCA.numrecover) * 100)
+              curData.positiveRate = ((curData.newCasesToday / curData.newTested) * 100)
+            }
+            prevDataCA = curData
+            resultCA.push(curData)
+          }
         }
       })
       .on('end', () => {
-        const length = result.length
-        console.log(result[length - 1].numconf)
-        const bcToday = result[length - 2]
-        const bcYesterday = result[length - 4]
-        const caToday = result[length - 1]
-        const caYesterday = result[length - 3]
-        const newCasesToday = bcToday.numconf - bcYesterday.numconf
-        const newRecover = bcToday.numrecover - bcYesterday.numrecover
-        const newTested = bcToday.numtested - bcYesterday.numtested
-        const newCasesTodayca = caToday.numconf - caYesterday.numconf
-        const newRecoverca = caToday.numrecover - caYesterday.numrecover
-        const newTestedca = caToday.numtested - caYesterday.numtested
         const data = {
-          bc: {
-            newCaseToday: newCasesToday,
-            percentIncreaseCase: ((newCasesToday / bcYesterday.numconf) * 100).toFixed(2),
-            newRecover: newRecover,
-            percentIncraseRec: ((newRecover / bcYesterday.numrecover) * 100).toFixed(2),
-            numTested: newTested,
-            positiveRate: ((newCasesToday / newTested) * 100).toFixed(2)
-          },
-          ca: {
-            newCaseToday: newCasesTodayca,
-            percentIncreaseCase: ((newCasesTodayca / caYesterday.numconf) * 100).toFixed(2),
-            newRecover: newRecoverca,
-            percentIncraseRec: ((newRecoverca / caYesterday.numrecover) * 100).toFixed(2),
-            numTested: newTestedca,
-            positiveRate: ((newCasesTodayca / newTestedca) * 100).toFixed(2)
-          }
+          bc: resultBC[resultBC.length - 1],
+          ca: resultCA[resultCA.length - 1]
         }
+        // data.bc.avgDailyCases = calculateAverage(resultBC, 'newCasesToday')
+        // data.bc.avgDailyRecover = calculateAverage(resultBC, 'newRecover')
+        // data.bc.avgTested = calculateAverage(resultBC, 'newTested')
+        // data.bc.avgDailyCasePercent = calculateAverage(resultBC, 'percentCase')
+        // data.bc.avgDailyRecPercent = calculateAverage(resultBC, 'percentRec')
+        data.bc.avgPosRate = calculateAverage(resultBC, 'positiveRate')
+        data.bc.avgDailyCaseIncasePercent = calculateAverage(resultBC, 'percentCase')
         resolve(data)
       })
   })
